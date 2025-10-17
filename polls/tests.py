5c1e0a9471158ebe4b81d8ac5d809b95ffe6752e
Heet Mehta <mehtaheet5@gmail.com>
2025-10-17 08:54:04 -0400

    Clean up before triggering Travis build

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class SmokeTests(TestCase):
+    def test_index_loads(self):
+        client = Client()
+        response = client.get("/polls/")
+        self.assertEqual(response.status_code, 200)
